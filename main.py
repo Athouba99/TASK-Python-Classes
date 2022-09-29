ab559c678df04b6ac1077fc1b6fcc3f7233d8b23
athouba99 <athooba99@gmail.com>
2022-09-29 03:22:52 +0300

    class task

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
 class Wallet:
-    def __init__(self, money):
-        pass
+    def __init__(self, money): #money 
+       self.money = money 
 
-    def credit(amount):
-        pass
 
-    def debit(amount):
-        pass
+    def credit(slef,amount):
+       
+       self.money =+ amount 
+       return credit 
 
+    def debit(self,amount):
+       self.mony =- debit
+       return debit
+
+default = 0 
 
 wallet = Wallet(6)
-wallet = Wallet()  # This should default money inside the wallet to 0
+wallet = Wallet(default)  # This should default money inside the wallet to 0
 print(wallet)
 
 
 class Person:
     # Implement the code here
-    pass
+   def __init__(self, name,location,money):
+    self.name = name 
+    self.location = location
+    self.wallet =  Wallet(money)
 
+    def moveTo(self,point):
+        self.location = point 
+        print("you changed your location to {point}")
 
 person = Person("Moh", 5, 50)
 
 
-class Vendor:
+class Vendor(Person):
     # implement Vendor!
-    pass
+    def __init__(self,name,range,price,location,money):
+        super().__init__(name,location,money)
+        self.range = 5 
+        self.price = 1
+
+    def sellTo(self,customer, number_of_icecreams):
+        self.location = update()
+        vendor_money = money
+    print("you sold 15")
 
 
-vendor = Vendor("Abdallah", 3, 6)
+vendor = Vendor("Abdallah", 3, 6,8,20)
 
 
 class Customer:
-    # implement Customer!
-    pass
+    # plan on blue note
+    def __init__(self): 
+
+
+    def _is_in_range(self,vendor):
+    if customer is in range:
+
+
+    def _have_enough_money(self,vendor, number_of_icecreams):    
+    
+
+    def request_icecream(self,vendor, number_of_icecreams):
+
+
 
 
 customer = Customer("Abdallah", 3, 6)
